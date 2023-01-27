744f17e98f697310830ac45a1a6ad480da0e844a
ayushranjan-11 <ayush.ranjan921@gmail.com>
2023-01-27 18:47:45 +0530

    Initial Commit

--- src/RahulShettyLinkTest.java
+++ src/RahulShettyLinkTest.java
@@ -1,12 +1,9 @@
-
-import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+
+import java.time.Duration;
 
 public class RahulShettyLinkTest {
 
